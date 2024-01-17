0076b8ac547ea7338b6773b41632b98d846d0e0f
springhana <xksxksanfro7@naver.com>
2024-01-18 06:26:31 +0900

    ✨feat:  footer 컴포넌트 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,15 @@ import '@/styles/globals.css';
 
 import { AppProps } from 'next/app';
 
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
