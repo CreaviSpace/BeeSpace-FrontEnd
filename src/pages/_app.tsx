ae808e81875d946549b01df3d53a6489c5ae0a77
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-17 22:37:33 +0900

    ✨feat:  header 컴포넌트 생성
    - header 페이지 생성
    - UI 구현 진행중
    
    Related to: header 컴포넌트 제작 #17

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,12 @@ import '@/styles/globals.css';
 
 import { AppProps } from 'next/app';
 
+import Header from '@/components/Header';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Header></Header>
       <Component {...pageProps} />
     </div>
   );
