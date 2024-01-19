5d7164523e77c721fa59c668a9a96622dc48edeb
springhana <xksxksanfro7@naver.com>
2024-01-20 00:48:31 +0900

    ✨feat:  마감 모집 컴포넌트 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Banner from '@/components/banner/Banner';
+import DeadLine from '@/components/DeadLine';
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
+    <main className="w-max_w mx-auto overflow-hidden">
       <Banner />
+      <DeadLine />
     </main>
   );
 }
