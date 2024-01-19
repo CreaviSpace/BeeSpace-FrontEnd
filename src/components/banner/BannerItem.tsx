b9894d36141399115151627d22e533a28e5a5d13
springhana <xksxksanfro7@naver.com>
2024-01-20 00:37:18 +0900

    🔨modify:  버튼 컴포넌트 변경

--- src/components/banner/BannerItem.tsx
+++ src/components/banner/BannerItem.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-import Button from '../button/Button';
+import CustomButton from '../CustomButton';
 
 interface IBannerProps {
   type: boolean;
@@ -20,6 +21,12 @@ export default function BannerItem({
   skill,
   date,
 }: IBannerProps) {
+  const router = useRouter();
+
+  const handleUrlMove = () => {
+    router.push(`/${type}/1`);
+  };
+
   return (
     <div className="max-w-[1200px] h-[350px] bg-[#C3D7D9] rounded-[20px] mx-auto p-10 flex">
       {image && (
@@ -35,19 +42,20 @@ export default function BannerItem({
 
       <div className="relative pl-5 pt-5 w-full ">
         <div className="border-b border-black pb-5">
+          {date && <div>{date}</div>}
           <div className="mb-5 text-bs_34">{title}</div>
           <div className="text-bs_20 line-clamp-3 ">{content}</div>
         </div>
         <div className="absolute right-0 bottom-0 mt-5">
-          <Button
-            content="자세히 보기"
-            link="/#"
-            paddingX={'px-5'}
-            paddingY={'py-1'}
-            rounded={'rounded-[20px]'}
-            color="bg-blue30"
-          />
+          <CustomButton
+            color="secondary"
+            className="px-10 py-2 rounded-[1.25rem]"
+            onClick={handleUrlMove}>
+            자세히 보기
+          </CustomButton>
         </div>
+
+        {skill && <div>스킬</div>}
       </div>
     </div>
   );
