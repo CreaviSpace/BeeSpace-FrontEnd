cc3fc25ab9225a736ecaa89d4b76c16ebf4e7fc1
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-20 00:48:11 +0900

    ✨feat:  좋아요 버튼 구현

---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,39 @@
+import { FaHeart } from '@react-icons/all-files/fa/FaHeart';
+import { FaRegHeart } from '@react-icons/all-files/fa/FaRegHeart';
+import { useState } from 'react';
+
+interface LikeButtonProps {
+  color?: string;
+  size?: number;
+}
+
+export default function LikeButton({
+  color = '#FF0000',
+  size = 20,
+}: LikeButtonProps) {
+  const [isBookmarked, setIsBookmarked] = useState(true);
+
+  const handleToggleBookmark = () => {
+    setIsBookmarked((prev) => !prev);
+  };
+
+  return (
+    <>
+      {isBookmarked ? (
+        <button
+          type="button"
+          aria-label="북마크 비활성화"
+          onClick={handleToggleBookmark}>
+          <FaRegHeart size={size} />
+        </button>
+      ) : (
+        <button
+          type="button"
+          aria-label="북마크 활성화"
+          onClick={handleToggleBookmark}>
+          <FaHeart color={color} size={size} />
+        </button>
+      )}
+    </>
+  );
+}
