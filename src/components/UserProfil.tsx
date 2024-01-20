a21ec960d4e96d0e2d40f985cd496a9f6be7f90e
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-20 17:53:04 +0900

    🔨modify:  사용자 프로필 props 추가

--- src/components/UserProfil.tsx
+++ src/components/UserProfil.tsx
@@ -1,10 +1,13 @@
 import { FaUserCircle } from '@react-icons/all-files/fa/FaUserCircle';
+// import Image from 'next/image';
 
 interface UserCircleProps {
   size?: number;
   color?: string;
   children?: string;
   className?: string;
+  userName?: string;
+  imageURL?: string;
 }
 
 export default function UserProfil({
@@ -12,13 +15,16 @@ export default function UserProfil({
   color = '#AEB4B7',
   children,
   className,
+  userName,
+  // imageURL,
 }: UserCircleProps) {
   return (
     <button
       className={`${className} flex justify-between w-fit items-center cursor-pointer rounded-[.3125rem] border-gray20`}>
       <div
-        aria-label={`${children} 프로필 사진`}
+        aria-label={`${userName} 프로필 사진`}
         className="bg-white rounded-full">
+        {/* <Image src={imageURL} alt={`${userName} 프로필 사진`}  className="w-32 h-auto" /> */}
         <FaUserCircle color={color} size={size} />
       </div>
       <div className="text-gray40 pl-2">{children}</div>
