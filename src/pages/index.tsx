e0b09ec48bf93de9e3c4bb521e0236e8d5492a3c
springhana <xksxksanfro7@naver.com>
2024-01-23 00:21:40 +0900

    ✨feat:  모집 카드 컴포넌트 구현 - 모집 카드 컴포넌트 - 북마크 추가  Resolves: #41

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Banner from '@/components/banner/Banner';
 import DeadLineRecruitment from '@/components/DeadLineRecruitment';
 import PopularProject from '@/components/PopularProject';
 import CommunityCardStyle from '@/components/style/CommunityCardStyle';
+import RecruitmentCardStyle from '@/components/style/RecruitmentCardStyle';
 
 import ProjectCardStyle from './../components/style/ProjectCardStyle';
 
@@ -9,12 +10,13 @@ export default function Home() {
   return (
     <main className="mx-auto overflow-hidden">
       <Banner />
-      <section className="max-w-max_w h-fit flex justify-between items-center mx-auto">
+      <section className="max-w-max_w h-fit grid grid-cols-2 mx-auto">
         <PopularProject />
         <DeadLineRecruitment />
       </section>
       <section className="max-w-max_w h-fit mx-auto py-16">
         <ProjectCardStyle />
+        <RecruitmentCardStyle />
         <CommunityCardStyle isActive="main" />
       </section>
     </main>
