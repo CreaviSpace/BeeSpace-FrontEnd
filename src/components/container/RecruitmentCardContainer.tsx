ca2f9a3c3e1e5185c6d46c6bd9c10b17b175ccc5
springhana <xksxksanfro7@naver.com>
2024-01-24 03:45:44 +0900

    🔨modify:  border 제거

--- src/components/container/RecruitmentCardContainer.tsx
+++ src/components/container/RecruitmentCardContainer.tsx
@@ -5,7 +5,7 @@ import RecruitmentCard from '../card/RecruitmentCard';
 export default function RecruitmentCardContainer() {
   const data = [1, 2, 3, 4, 5, 6];
   return (
-    <div className="max-w-max_w my-10 border">
+    <div className="max-w-max_w my-10">
       <div className="grid grid-cols-3 gap-y-6 gap-x-3 bg-white tablet:grid-cols-2 mobile:grid-cols-1">
         {data.map((item) => (
           <RecruitmentCard
