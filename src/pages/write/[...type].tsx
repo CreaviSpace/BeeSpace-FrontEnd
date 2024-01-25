c01d9c8369cd2eb43ce4d1671bac8cd3317ad5c7
springhana <xksxksanfro7@naver.com>
2024-01-26 06:30:22 +0900

    🔨modify:  조건 추가 - 모집 일때만 모집 글쓰기

--- src/pages/write/[...type].tsx
+++ src/pages/write/[...type].tsx
@@ -1,9 +1,13 @@
+import { useRouter } from 'next/router';
+
 import RecruitmentWrite from '@/components/write/RecruitmentWrite';
 
 export default function Write() {
+  const router = useRouter();
+  const { type } = router.query;
   return (
     <div className="py-5">
-      <RecruitmentWrite />
+      {type && type[0] === 'recruitment' && <RecruitmentWrite />}
     </div>
   );
 }
