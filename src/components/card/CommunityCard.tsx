1efbf6030c49b05420488f314197988b9ca12e1b
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-26 18:09:37 +0900

    🎨design:   커뮤니티, 프로젝트 카드 UI 수정
    - 좋아요 버튼, 북마크 버튼 클릭시 디테일 페이지로 이동하는 문제 수정
    
    프로젝트 카드 컴포넌트 구현 #37
    커뮤니티 카드 컴포넌트 구현 #29

--- src/components/card/CommunityCard.tsx
+++ src/components/card/CommunityCard.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 import LikeButton from '@/components/button/LikeButton';
 import UserProfilButton from '@/components/button/UserProfilButton';
 
 interface CommunityCardProps {
+  id?: string;
+  type?: string;
   userName: string;
   date: string;
   contents: string;
@@ -10,6 +14,8 @@ interface CommunityCardProps {
   className?: string;
 }
 export default function CommunityCard({
+  id,
+  type,
   userName,
   date,
   contents,
@@ -17,25 +23,23 @@ export default function CommunityCard({
   comments,
   className,
 }: CommunityCardProps) {
-  const data = ['1', '2', '3', '4'];
   return (
     <>
-      {data?.map((item, index) => (
-        <div
-          className={`${className} w-full py-5 px-7 rounded-bs_5 bg-white border-gray10`}
-          key={`card-${index}`}>
-          {/* 사용자 정보, 게시글 날짜 및 시간 */}
-          <div className="pb-2 flex items-center justify-between">
-            <UserProfilButton userName={userName} className="">
-              {userName}
-            </UserProfilButton>
-            <span className="text-bs_14 text-gray20">
-              <time dateTime={date}>{date}</time>
-            </span>
-          </div>
-          <hr className="w-full m-auto" aria-hidden />
-          {/* 게시글 내용, 조회수, 댓글, 좋아요 */}
-          <div className="flow-root pt-4">
+      <div
+        className={`${className} w-full py-5 px-7 rounded-bs_5 bg-white border-gray10`}>
+        {/* 사용자 정보, 게시글 날짜 및 시간 */}
+        <div className="pb-2 flex items-center justify-between">
+          <UserProfilButton userName={userName} className="">
+            {userName}
+          </UserProfilButton>
+          <span className="text-bs_14 text-gray20">
+            <time dateTime={date}>{date}</time>
+          </span>
+        </div>
+        <hr className="w-full m-auto" aria-hidden />
+        {/* 게시글 내용, 조회수, 댓글, 좋아요 */}
+        <div className="flow-root pt-4">
+          <Link href={`${type}/${id}`}>
             <p className="overflow-hidden text-ellipsis break-keep line-clamp-2">
               {contents}
             </p>
@@ -47,10 +51,10 @@ export default function CommunityCard({
                 댓글 <span>{comments}</span>
               </p>
             </div>
-            <LikeButton className="pt-4 float-end" />
-          </div>
+          </Link>
+          <LikeButton className="pt-4 float-end" />
         </div>
-      ))}
+      </div>
     </>
   );
 }
--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
+import Bookmark from '@/components/button/Bookmark';
 import LikeButton from '@/components/button/LikeButton';
 import Tag from '@/components/Tag';
 
-import Bookmark from './../button/Bookmark';
-
-interface ProjectCardProps {
+interface IProjectCardProps {
+  id?: string;
+  type?: string;
   image?: string;
   title: string;
   content: string;
@@ -14,39 +16,44 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard({
+  id,
+  type,
   image,
   title,
   content,
   tagName,
   tagCategory,
-}: ProjectCardProps) {
+}: IProjectCardProps) {
   return (
-    <div className="relative w-full border border-gray10 rounded-b-bs_20">
-      <Tag
-        name={tagName}
-        category={tagCategory}
-        className="absolute right-3 top-3"
-      />
-      {image && (
-        <div className="relative overflow-hidden w-full h-[180px]">
-          <Image
-            src={image}
-            alt="임시 프로젝트 이미지"
-            // layout="responsive"
-            fill
-            className="object-cover object-top -z-[1] "
-          />
-        </div>
-      )}
+    <div className="relative w-full h-[380px] border border-gray10 rounded-b-bs_20">
+      <Link href={`${type}/${id}`}>
+        <Tag
+          name={tagName}
+          category={tagCategory}
+          className="absolute top-3 left-3"
+        />
+        {image && (
+          <div className="relative overflow-hidden w-full h-[180px]">
+            <Image
+              src={image}
+              alt="임시 프로젝트 이미지"
+              fill
+              className="object-cover object-top -z-[1] "
+            />
+          </div>
+        )}
+      </Link>
       <div className="w-full h-[200px] p-5 rounded-b-bs_20 relative overflow-hidden">
         <Bookmark
           className="absolute -top-[.3125rem] right-[.375rem]"
           size={35}
         />
-        <h3 className="text-bs_18 pb-3 font-bold">{title}</h3>
-        <p className="overflow-hidden text-ellipsis break-keep line-clamp-2">
-          {content}
-        </p>
+        <Link href={`${type}/${id}`}>
+          <h3 className="text-bs_18 pb-3 font-bold">{title}</h3>
+          <p className="overflow-hidden text-ellipsis break-keep line-clamp-2">
+            {content}
+          </p>
+        </Link>
       </div>
       <LikeButton className="absolute bottom-3 left-5" />
     </div>
