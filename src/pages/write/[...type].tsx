6b93a92a20ad643e6f4a8c150ca57735ff5d48cb
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-27 20:03:40 +0900

    ✨feat:  커뮤니티 글쓰기 페이지 추가
    커뮤니티 글쓰기 페이지 구현 #54

--- src/pages/write/[...type].tsx
+++ src/pages/write/[...type].tsx
@@ -1,13 +1,20 @@
 import { useRouter } from 'next/router';
 
+import CommunityWrite from '@/components/write/CommunityWrite';
 import RecruitmentWrite from '@/components/write/RecruitmentWrite';
 
 export default function Write() {
   const router = useRouter();
   const { type } = router.query;
+
+  if (!type) {
+    return <div>잘못된 페이지</div>;
+  }
+
   return (
     <div className="py-5">
-      {type && type[0] === 'recruitment' && <RecruitmentWrite />}
+      {type[0] === 'recruitment' && <RecruitmentWrite />}
+      {type[1] === 'community' && <CommunityWrite />}
     </div>
   );
 }
