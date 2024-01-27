8695be68d3afb1513945d0fccc4fb9f17e9f2f91
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-27 19:47:56 +0900

    ✨feat: 인기태그 컴포넌트 임시 UI 구현
    인기 태그 컴포넌트 구현 #52

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/banner/Banner';
 import Category from '@/components/Category';
 import CommunityCardStyle from '@/components/container/CommunityCardContainer';
+import PopularTag from '@/components/PopularHashTag';
 export default function Community() {
   const categories = [
     {
@@ -23,7 +24,8 @@ export default function Community() {
         <Banner />
       </section>
       <Category category={categories} btnValue="커뮤니티 글쓰기"></Category>
-      <section className="max-w-max_w flex justify-between pt-3 pb-24">
+      <section className="max-w-max_w flex justify-between pt-3 pb-24 m-auto">
+        <PopularTag />
         <CommunityCardStyle />
       </section>
     </main>
