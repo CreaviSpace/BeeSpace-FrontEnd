fd80a76dbd5d0e9f08fc434ab2083f9b9c7166d9
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-27 19:49:12 +0900

    🔨modify:  커뮤니티 카드 컴포넌트 수정
    - 사용자프로필 컴포넌트 부분 수정
    - 링크 수정
    
    커뮤니티 카드 컴포넌트 구현 #29

--- src/components/card/CommunityCard.tsx
+++ src/components/card/CommunityCard.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import LikeButton from '@/components/button/LikeButton';
 import UserProfilButton from '@/components/button/UserProfilButton';
+import Tag from '@/components/Tag';
 
 interface CommunityCardProps {
   id?: string;
@@ -29,9 +30,7 @@ export default function CommunityCard({
         className={`${className} w-full py-5 px-7 rounded-bs_5 bg-white border-gray10`}>
         {/* 사용자 정보, 게시글 날짜 및 시간 */}
         <div className="pb-2 flex items-center justify-between">
-          <UserProfilButton userName={userName} className="">
-            {userName}
-          </UserProfilButton>
+          <UserProfilButton userName={userName} />
           <span className="text-bs_14 text-gray20">
             <time dateTime={date}>{date}</time>
           </span>
@@ -39,10 +38,15 @@ export default function CommunityCard({
         <hr className="w-full m-auto" aria-hidden />
         {/* 게시글 내용, 조회수, 댓글, 좋아요 */}
         <div className="flow-root pt-4">
+          <div>
+            <Link href={`${type}/${id}`}>
+              <p className="overflow-hidden text-ellipsis break-keep line-clamp-2 mb-3">
+                {contents}
+              </p>
+            </Link>
+            <Tag name="example" category="hashtag" />
+          </div>
           <Link href={`${type}/${id}`}>
-            <p className="overflow-hidden text-ellipsis break-keep line-clamp-2">
-              {contents}
-            </p>
             <div className="pt-4 float-start flex gap-x-3 text-bs_14">
               <p>
                 조회수 <span>{views}</span>
