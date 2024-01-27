710a230dced95b964be54f0f806ceac0233f477d
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-27 19:32:53 +0900

    🔨modify:  사이드 버튼 컴포넌트 수정
    - 컴포넌트 사용시 부모 요소에 relative 만 추가하도록 수정
    
    사이드 버튼 컴포넌트 구현 #49

--- src/components/button/SideButton.tsx
+++ src/components/button/SideButton.tsx
@@ -10,24 +10,26 @@ interface ISideButtonProps {
 
 export default function SideButton({ className }: ISideButtonProps) {
   return (
-    <aside
-      className={`${className} fixed mobile:hidden w-fit h-fit flex flex-col items-center gap-2 p-2 py-3 rounded-md bg-secondary`}>
-      <LikeButton
-        color="black"
-        size={25}
-        className="border rounded-md bg-white p-3"
-      />
-      <Bookmark
-        color="black"
-        size={25}
-        className="border rounded-md bg-white p-3"
-      />
-      <button
-        type="button"
-        aria-label="공유 버튼"
-        className="border rounded-md bg-white p-3">
-        <FaShareAlt size={25} className="m-auto" />
-      </button>
+    <aside className="absolute -right-2 top-1/3">
+      <div
+        className={`${className} fixed mobile:hidden w-fit h-fit flex flex-col items-center gap-2 p-2 py-3 rounded-md bg-secondary`}>
+        <LikeButton
+          color="black"
+          size={25}
+          className="border rounded-md bg-white p-3"
+        />
+        <Bookmark
+          color="black"
+          size={25}
+          className="border rounded-md bg-white p-3"
+        />
+        <button
+          type="button"
+          aria-label="공유 버튼"
+          className="border rounded-md bg-white p-3">
+          <FaShareAlt size={25} className="m-auto" />
+        </button>
+      </div>
     </aside>
   );
 }
