9ad4c2a7aae49df32fef5a72fd17b8ad12451301
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-30 00:29:58 +0900

    🔨modify:  사용자 로그인 헤더 추가
    header 컴포넌트 제작 #17

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 import CustomButton from '@/components/button/CustomButton';
+import LogInUserHeader from '@/components/LogInUserHeader';
 
 interface MenuItem {
   name: string;
@@ -12,6 +13,7 @@ interface MenuItem {
 
 export default function Header() {
   const [value, setValue] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
   const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   const menu: Array<MenuItem> = [
@@ -24,25 +26,30 @@ export default function Header() {
     setIsSearchVisible(!isSearchVisible);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSignInOut = () => {
+    setIsLogin(!isLogin);
+  };
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   return (
     <header className="sticky top-0 w-full h-16 z-20 bg-white">
-      <nav className="border-b border-gray10">
+      <nav className="border-b border-gray10 ">
         <div className="flex justify-between max-w-max_w content-center m-auto items-center">
           <h1>
             <Link href="/">
               <Image
                 src="/BS_Logo500x181.png"
                 alt="비스페이스 로고"
-                width={128}
-                height={46}
+                width={500}
+                height={200}
+                className="w-32 h-auto"
               />
             </Link>
           </h1>
-          <ul className="flex">
+          <ul className="flex mobile:hidden">
             {menu.map((item, index) => (
               <li key={index}>
                 <Link
@@ -53,30 +60,39 @@ export default function Header() {
               </li>
             ))}
           </ul>
-          <ul className="flex items-center">
-            <li
+          <div className="flex items-center">
+            <div
               className="p-5 text-center block cursor-pointer"
               aria-label="검색 버튼"
               onClick={handleSearchToggle}>
               <AiOutlineSearch size={22} />
-            </li>
-            <li>
-              <CustomButton className="py-2 px-4 mr-3" color="primary">
-                로그인
-              </CustomButton>
-            </li>
-            <li>
-              <CustomButton className="py-2 px-3">회원가입</CustomButton>
-            </li>
-          </ul>
+            </div>
+            {isLogin ? (
+              <ul className="flex">
+                <li>
+                  <CustomButton
+                    onClick={handleSignInOut}
+                    className="py-2 px-4 mr-3"
+                    color="primary">
+                    로그인
+                  </CustomButton>
+                </li>
+                <li>
+                  <CustomButton className="py-2 px-3">회원가입</CustomButton>
+                </li>
+              </ul>
+            ) : (
+              <LogInUserHeader onClick={handleSignInOut} />
+            )}
+          </div>
         </div>
       </nav>
       {isSearchVisible && (
-        <div className="h-screen fixed w-full z-10">
+        <div className="h-screen fixed w-full z-10 ">
           <div className="h-20 shadow-md py-4 bg-white">
             <form
               onSubmit={(e) => e.preventDefault()}
-              className="max_w h-full m-auto relative max-w-max_w">
+              className="max_w h-full m-auto relative max-w-max_w mobile:px-3">
               <label htmlFor="searchValue" id="searchValue" className="sr-only">
                 검색창
               </label>
@@ -87,7 +103,7 @@ export default function Header() {
                 id="searchValue"
                 placeholder="검색어를 입력하세요"
                 className="w-full h-full bg-[#F5F5F5] rounded-bs_5 absolute p-5"
-                onChange={handleChange}
+                onChange={handleValueChange}
               />
               <button
                 type="submit"
