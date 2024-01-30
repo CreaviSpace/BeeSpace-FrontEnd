e9bd8c40d82c7df52381dc9694fe61a4a3489f11
rerro <101159509+rorre0@users.noreply.github.com>
2024-01-31 00:17:02 +0900

    ✨feat:  프로젝트 글쓰기 페이지 추가
    프로젝트 글쓰기 페이지 구현 #59

---
+++ src/components/write/ProjectWrite.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ProjectWrite() {
+  return (
+    <div className="max-w-max_w m-auto p-20">
+      <h1 className="text-center text-[2rem] font-bold">
+        프로젝트를 소개해주세요
+      </h1>
+    </div>
+  );
+}
