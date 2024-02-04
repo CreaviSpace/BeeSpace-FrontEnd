53007be8153a743d2ed76f646790e1c8f58a8188
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-05 00:01:28 +0900

    ♻️refactor:  로그인, 회원가입 모달 리팩토링
    모달창 컴포넌트 구현 #58

--- src/components/modals/LogInModal.tsx
+++ src/components/modals/LogInModal.tsx
@@ -12,39 +12,46 @@ import Modals from './Modals';
 export default function LogInModal() {
   const { isOpen, onOpen, onClose } = useLoginModal();
 
+  const icons = [
+    { icon: <FcGoogle key="google" size={45} />, style: 'bg-white', link: '/' },
+    {
+      icon: <RiKakaoTalkFill key="kakao" size={45} />,
+      style: 'bg-yellow-300',
+      link: '/',
+    },
+    {
+      icon: <IoLogoGithub key="github" size={45} />,
+      style: 'bg-white',
+      link: '/',
+    },
+    {
+      icon: (
+        <Image
+          src="/img/icon/SiNaver.svg"
+          alt="네이버 아이콘"
+          width={35}
+          height={35}
+          className="object-center"
+        />
+      ),
+      style: 'bg-green-500 p-3 flex',
+      link: '/',
+    },
+  ];
   return (
     <Modals isOpen={isOpen} onClose={onClose}>
       <ModalCloseButton />
       <ModalHeader className="text-center mt-10 mb-2">로그인</ModalHeader>
       <ModalBody>
         <section className="mb-11 m-auto w-fit flex justify-between gap-3">
-          <Link
-            href="/"
-            className="bg-white rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <FcGoogle size={45} />
-          </Link>
-          <Link
-            href="/"
-            className="bg-yellow-300 rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <RiKakaoTalkFill size={45} />
-          </Link>
-          <Link
-            href="/"
-            className="bg-white rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <IoLogoGithub size={45} />
-          </Link>
-
-          <Link
-            href="/"
-            className="bg-green-500 rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-3 flex">
-            <Image
-              src="/img/icon/SiNaver.svg"
-              alt="네이버 아이콘"
-              width={35}
-              height={35}
-              className="object-center"
-            />
-          </Link>
+          {icons.map((icon, index) => (
+            <Link
+              key={`icon-${index}`}
+              href={icon.link}
+              className={`${icon.style} rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2`}>
+              {icon.icon}
+            </Link>
+          ))}
         </section>
       </ModalBody>
     </Modals>
--- src/components/modals/SignUpModal.tsx
+++ src/components/modals/SignUpModal.tsx
@@ -11,39 +11,46 @@ import Modals from './Modals';
 
 export default function SignUpModal() {
   const { isOpen, onOpen, onClose } = useSignUpModal();
-
+  const icons = [
+    { icon: <FcGoogle key="google" size={45} />, style: 'bg-white', link: '/' },
+    {
+      icon: <RiKakaoTalkFill key="kakao" size={45} />,
+      style: 'bg-yellow-300',
+      link: '/',
+    },
+    {
+      icon: <IoLogoGithub key="github" size={45} />,
+      style: 'bg-white',
+      link: '/',
+    },
+    {
+      icon: (
+        <Image
+          src="/img/icon/SiNaver.svg"
+          alt="네이버 아이콘"
+          width={35}
+          height={35}
+          className="object-center"
+        />
+      ),
+      style: 'bg-green-500 p-3 flex',
+      link: '/',
+    },
+  ];
   return (
     <Modals isOpen={isOpen} onClose={onClose}>
       <ModalHeader className="text-center mt-10 mb-2">회원가입</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
         <section className="mb-11 m-auto w-fit flex justify-between gap-3">
-          <Link
-            href="/signup"
-            className="bg-white rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <FcGoogle size={45} />
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-yellow-300 rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <RiKakaoTalkFill size={45} />
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-white rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2">
-            <IoLogoGithub size={45} />
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-green-500 rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-3 flex">
-            <Image
-              src="/img/icon/SiNaver.svg"
-              alt="네이버 아이콘"
-              width={35}
-              height={35}
-              className="object-center"
-            />
-          </Link>
+          {icons.map((icon, index) => (
+            <Link
+              key={`icon-${index}`}
+              href={icon.link}
+              className={`${icon.style} rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2`}>
+              {icon.icon}
+            </Link>
+          ))}
         </section>
       </ModalBody>
     </Modals>
