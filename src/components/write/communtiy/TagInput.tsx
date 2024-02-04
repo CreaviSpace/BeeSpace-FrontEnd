61a62dfedb400d2ee0b422f1c2f86ee7edec7488
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-05 01:03:20 +0900

    🔨modify:  커뮤니티 글쓰기 수정
    - 태그 Ui 및 기능 수정
    - 제목 스타일링 수정
    
    커뮤니티 글쓰기 페이지 구현 #54

--- src/components/write/communtiy/TagInput.tsx
+++ src/components/write/communtiy/TagInput.tsx
@@ -1,8 +1,11 @@
-import { SetStateAction, useState } from 'react';
+import { SetStateAction, useEffect, useRef, useState } from 'react';
+
+import CustomButton from '@/components/button/CustomButton';
 
 export default function TagInput() {
   const [inputValue, setInputValue] = useState('');
-  const [displayedValue, setDisplayedValue] = useState('');
+  const [displayedValues, setDisplayedValues] = useState<string[]>([]);
+  const inputRef = useRef(null);
 
   const handleInputChange = (e: {
     target: { value: SetStateAction<string> };
@@ -10,27 +13,57 @@ export default function TagInput() {
     setInputValue(e.target.value);
   };
 
-  const handleEnterKey = (e: { key: string }) => {
+  const handleInputKey = (e: { key: string }) => {
+    // 'Enter' 키 입력시
     if (e.key === 'Enter') {
-      setDisplayedValue(inputValue);
+      // value 값 배열 생성
+      setDisplayedValues((prevValues) => [...prevValues, inputValue]);
+      // input 태그 value 삭제
+      setInputValue('');
+    } else if (e.key === ',') {
+      // ',' 입력시 쉼표를 제거하고 value 값 배열 생성
+      setDisplayedValues((prevValues) => [
+        ...prevValues,
+        inputValue.replace(/,/g, ''),
+      ]);
+      // input 태그 value 삭제
+      setInputValue('');
     }
   };
 
+  const handleDeleteButton = (index: number) => {
+    // 현재 상태의 배열을 업데이트
+    setDisplayedValues(
+      (prevValues) => prevValues.filter((_, i) => i !== index)
+      // => 특정 index(i)에 해당하는 요소를 제외한 새로운 배열을 생성
+    );
+  };
+
+  useEffect(() => {}, [displayedValues]);
+
   return (
-    <div className="relative">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyPress={handleEnterKey}
-        placeholder="태그를 입력해주세요."
-        className="w-full h-[3.125rem] px-5 border border-gary10 rounded-bs_5"
-      />
-      {displayedValue && (
-        <div className="mt-2 top-0 left-3">
-          <p>{displayedValue}</p>
-        </div>
-      )}
+    <div className="w-full">
+      <div className="flex items-center">
+        {displayedValues.map((value, index) => (
+          <div key={index} className="flex justify-between mr-1">
+            <CustomButton
+              onClick={() => handleDeleteButton(index)}
+              color="hashtag"
+              className="max-h-[2rem] px-2 text-bs_14">
+              {value} <button>X</button>
+            </CustomButton>
+          </div>
+        ))}
+        <input
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyUp={handleInputKey}
+          placeholder="태그를 입력해주세요."
+          className="w-full h-[3.125rem] px-3 border border-gary30 rounded-bs_5 border-gray30"
+        />
+      </div>
     </div>
   );
 }
