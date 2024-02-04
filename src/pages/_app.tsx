2ac613e0754430b1a74970cca645008628368dfb
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-05 01:07:25 +0900

    ✨feat:  모달창 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,11 @@ import { AppProps } from 'next/app';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
+import LogInModal from '@/components/modals/LogInModal';
 import ReconfirmModal from '@/components/modals/ReconfirmModal';
 import ReportModal from '@/components/modals/ReportModal';
 import SearchErrorModal from '@/components/modals/SearchErrorModal';
+import SignUpModal from '@/components/modals/SignUpModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="relative h-full">
         <Header />
         <ReportModal />
+        <LogInModal />
+        <SignUpModal />
         <ReconfirmModal value="계정" />
         <SearchErrorModal />
         <Component {...pageProps} />
