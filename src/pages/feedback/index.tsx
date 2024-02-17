bdd0f59dfa6d959bb39c47111deeebabd795f893
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-18 00:37:01 +0900

    🔨modify:  피드백 코드 수정
    피드백 페이지 구현 #66

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -4,9 +4,9 @@ import MultipleChoiceQuestion from '@/components/feedback/MultipleChoiceQuestion
 import QuestionBox from '@/components/feedback/QuestionBox';
 import ShortAnswerQuestion from '@/components/feedback/ShortAnswerQuestion';
 
-export default function index() {
+export default function feedback() {
   return (
-    <main className="bg-blue10 py-10 flow-root">
+    <main className="bg-blue10 py-10 w-full h-full min-h-[calc(100vh-4rem-250px)]">
       <section className="max-w-[48rem] m-auto relative">
         <h1 className="text-3xl font-semibold mb-4">피드백</h1>
         <QuestionBox />
@@ -15,7 +15,7 @@ export default function index() {
           <MultipleChoiceQuestion />
           <CheckBoxQuestion />
         </div>
-        <div className="mt-7 float-end flex gap-2">
+        <div className="flex justify-end mt-8 gap-2">
           <CustomButton className="py-2 px-3 bg-white">취소</CustomButton>
           <CustomButton className="py-2 px-3" color="secondary">
             저장
