02fbc018868be1e467fa023a32769d126e4746f6
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-18 00:42:09 +0900

    🎨design:  프로필 페이지, 프로필 수정 페이지 스타일링 수정
    프로필/ 프로필 수정 페이지 구현 #63

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import CustomButton from '@/components/button/CustomButton';
 import UniversalCard from '@/components/card/UniversalCard';
 import Category from '@/components/Category';
+import Tag from '@/components/Tag';
 import { card, images } from '@/utils/data';
 
-import CustomButton from './../../components/button/CustomButton';
-
 export default function Profile() {
+  const skill = [
+    'Javascript',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+    'java',
+  ];
   const categories = [
     {
       name: '내 프로젝트',
@@ -32,40 +45,36 @@ export default function Profile() {
   ];
   const data = ['1', '2', '3'];
   return (
-    <main className="max-w-max_w m-auto relative flow-root">
-      <Link href={`/profile/123`} className="float-end mt-10">
-        <CustomButton className="px-2 py-1">수정</CustomButton>
-      </Link>
-      <section className="flex flex-col items-center gap-y-1 mt-[110px]">
+    <main className="relative flow-root">
+      <section className="max-w-screen-md m-auto mt-[4rem]">
         <h1 className="sr-only">내 프로필</h1>
-        <Image
-          src="/img/user/default.avif"
-          alt="유저 사진"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
-        <h2 className="text-bs_20 font-bold">User Name</h2>
-        <div className="text-bs_14 my-2">
-          <span>4년차</span>
-          <span className="font-bold text-bs_16"> 프론트엔드</span>
-        </div>
-        <p className="w-[300px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          error, temporibus quisquam repellat nesciunt minima omnis laboriosam
-          dolores! Ab ad error laborum pariatur delectus culpa optio natus
-          repellendus architecto iusto?
-        </p>
-        <div className="mt-3 flex gap-1 text-bs_14">
-          <span className="block border rounded-bs_20 border-black px-4">
-            Javascript
-          </span>
-          <span className="block border rounded-bs_20 border-black px-4">
-            Javascript
-          </span>
-          <span className="block border rounded-bs_20 border-black px-4">
-            Javascript
-          </span>
+        <Link href={`/profile/123`} className="flex justify-end mt-10">
+          <CustomButton className="px-2 py-1">수정</CustomButton>
+        </Link>
+        <div className="max-w-[22.4375rem] m-auto flex flex-col items-center gap-y-1">
+          <Image
+            src="/img/user/default.avif"
+            alt="유저 사진"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <h2 className="text-bs_20 font-bold">User Name</h2>
+          <div className="text-bs_14 my-2">
+            <span>4년차</span>
+            <span className="font-bold text-bs_16"> 프론트엔드</span>
+          </div>
+          <p className="break-all">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            error, temporibus quisquam repellat nesciunt minima omnis laboriosam
+            dolores! Ab ad error laborum pariatur delectus culpa optio natus
+            repellendus architecto iusto?
+          </p>
+          <div className="mt-3">
+            {skill.map((item, index) => (
+              <Tag key={index} name={item} category="skill" />
+            ))}
+          </div>
         </div>
       </section>
       <Category category={categories} />
