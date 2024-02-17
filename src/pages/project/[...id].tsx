3543634cfb2c954d277a55b05a1ec6e6928a34cb
Sunghwan Kim <97121074+springhana@users.noreply.github.com>
2024-02-17 21:17:19 +0900

    Update [...id].tsx

--- src/pages/project/[...id].tsx
+++ src/pages/project/[...id].tsx
@@ -37,9 +37,7 @@ export default function ProjectDetail() {
         title="Some Title"
         userName="author"
       />
-      <div className="absolute right-[2rem]">
         <SideButton />
-      </div>
       <div className="py-8 border-b border-gray10">
         <div dangerouslySetInnerHTML={{ __html: details.content }} />
       </div>
