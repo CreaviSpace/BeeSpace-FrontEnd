6040a593a7d5123d6cdc20ab2e3043ecc2e4e90c
rerro <101159509+rorre0@users.noreply.github.com>
2024-02-22 23:45:32 +0900

    🎨design:  글쓰기 페이지 ui 및 태그 수정

--- src/components/write/CommunityWrite.tsx
+++ src/components/write/CommunityWrite.tsx
@@ -25,8 +25,8 @@ export default function CommunityWrite() {
   ];
 
   return (
-    <main className="max-w-max_w m-auto p-20">
-      <section>
+    <main>
+      <section className="max-w-max_w m-auto p-20">
         <h1 className="text-center text-[2rem] font-bold">
           프로젝트를 소개해주세요
         </h1>
