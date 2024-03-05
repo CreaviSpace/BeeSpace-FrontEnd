7041c5a7c3e5052682b3619fc030baba6428651a
springhana <xksxksanfro7@naver.com>
2024-03-06 05:13:51 +0900

    🔨modify:  링크 데이터 추가 - props links로 이름 변경 및 값 변경

--- src/components/details/project/DistributeLink.tsx
+++ src/components/details/project/DistributeLink.tsx
@@ -3,23 +3,23 @@ import { FaPaperclip } from '@react-icons/all-files/fa/FaPaperclip';
 import Icons from '@/components/Icons';
 
 interface IDistributeLinkProps {
-  link: { title: string; link: string }[];
+  links: { type: string; url: string }[];
 }
 
-export default function DistributeLink({ link }: IDistributeLinkProps) {
+export default function DistributeLink({ links }: IDistributeLinkProps) {
   return (
     <section>
       <h3 className="text-bs_20 font-bold ml-4">링크</h3>
       <ul>
-        {link.map((item, index) => (
+        {links.map((item, index) => (
           <li
             key={`${item}-${index}`}
             className="flex items-center justify-between ml-8 p-5 border-l-2 border-gray10">
             <div className="flex items-center">
-              <Icons icon={item.title} />
+              <Icons icon={item.type} />
               <div className="ml-5">
-                <p className="text-bs_18 font-bold">{item.title}</p>
-                <div className="text-bs_16">{item.link}</div>
+                <p className="text-bs_18 font-bold">{item.type}</p>
+                <div className="text-bs_16">{item.url}</div>
               </div>
             </div>
             <span className="cursor-pointer">
