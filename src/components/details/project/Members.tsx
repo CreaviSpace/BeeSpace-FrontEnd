9d7035a62c0823de6a9ac87c930b13437973e829
springhana <xksxksanfro7@naver.com>
2024-03-06 05:11:50 +0900

    🔨modify:  Member 데이터 추가 - props positions으로 이름 변경

--- src/components/details/project/Members.tsx
+++ src/components/details/project/Members.tsx
@@ -1,22 +1,29 @@
 import UserProfileButton from '@/components/button/UserProfileButton';
 
 interface IMembersProps {
-  personnel: { personnel: string; people: { name: string }[] }[];
+  positions: {
+    position: string;
+    members: {
+      memberId: number;
+      memberProfile: string;
+      memberNickname: string;
+    }[];
+  }[];
 }
 
-export default function Members({ personnel }: IMembersProps) {
+export default function Members({ positions }: IMembersProps) {
   return (
     <section className="pt-5">
       <h3 className="text-bs_20 font-bold">팀원 소개</h3>
-      {personnel.map((item, index) => (
+      {positions.map((item, index) => (
         <ul
           key={`${item}-${index}`}
           className="flex items-center ml-8 p-5 border-l-2 border-gray10">
-          <li className="min-w-20">{item.personnel}</li>
-          {item.people.map((people, index) => (
+          <li className="min-w-20">{item.position}</li>
+          {item.members.map((people, index) => (
             <li key={`${people}-${index}`} className="ml-5">
               <UserProfileButton
-                userName={people.name}
+                userName={'author'}
                 className="border border-gray10 px-4 py-3"
               />
             </li>
