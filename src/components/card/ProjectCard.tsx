d3efd1f65e7de143b20e49e972a2e85734894ec4
springhana <xksxksanfro7@naver.com>
2024-03-06 05:09:11 +0900

    🔨modify:  프로젝트 페이지 데이터 추가 - 로딩 시에는 스켈레톤 UI 적용 - 다수의 Props를 item object로 묶어서 보냄

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -1,49 +1,34 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Bookmark from '@/components/button/Bookmark';
-import LikeButton from '@/components/button/LikeButton';
 import Tag from '@/components/Tag';
 
-import SkeletonProjectCard from '../skeleton/SkeletonProjectCard';
+import { IProjectType } from '../../types/global';
+import Icons from '../Icons';
 
 interface IProjectCardProps {
-  id?: string;
-  type?: string;
-  image?: string;
-  title: string;
-  content: string;
+  item: IProjectType;
   tagName: string;
   tagCategory: 'team' | 'individual' | 'hashtag' | 'field';
 }
 
 export default function ProjectCard({
-  id,
-  type,
-  image,
-  title,
-  content,
+  item,
   tagName,
   tagCategory,
 }: IProjectCardProps) {
-  const isImageAvailable = !!image;
-
-  if (false) {
-    return <SkeletonProjectCard />;
-  }
-
   return (
     <div className="relative max-w-md w-full m-auto h-[23.75rem] border border-gray10 rounded-b-bs_20">
-      <Link href={`${type}/${id}`}>
+      <Link href={`${item.kind}/${item.id}`}>
         <Tag
           name={tagName}
           category={tagCategory}
           className="absolute top-3 left-3"
         />
-        {image && (
+        {item.thumbnail && (
           <div className="relative overflow-hidden w-full h-[11.25rem]">
             <Image
-              src={image}
+              src={item.thumbnail}
               alt="임시 프로젝트 이미지"
               fill
               className="object-cover object-top -z-[1] "
@@ -52,24 +37,32 @@ export default function ProjectCard({
         )}
       </Link>
       <div className="w-full h-fit p-5 rounded-b-bs_20 relative overflow-hidden">
-        <Bookmark
+        {/* <Bookmark
           className="absolute -top-[.3125rem] right-[.375rem]"
           size={35}
-        />
-        <Link href={`${type}/${id}`}>
-          <h3 className="text-bs_18 pb-3 font-bold mt-5">{title}</h3>
+          id={item.id}
+          kind={item.kind}
+        /> */}
+        <Link href={`${item.kind}/${item.id}`}>
+          <h3 className="text-bs_18 pb-3 font-bold mt-5">{item.title}</h3>
           <p
-            className={`overflow-hidden text-ellipsis break-keep ${!isImageAvailable ? 'line-clamp-[10]' : 'line-clamp-2'}`}>
-            {content}
+            className={`overflow-hidden text-ellipsis break-keep ${!item.thumbnail ? 'line-clamp-[10]' : 'line-clamp-2'}`}>
+            {item.bannerContent}
           </p>
         </Link>
       </div>
       <div className="flow-root px-5">
-        <LikeButton className="float-start" />
+        {/* <LikeButton className="float-start" id={item.id} kind={item.kind} /> */}
         <ul className="float-end flex gap-1">
-          <li className="border border-gray20 rounded-full w-6 h-6"></li>
-          <li className="border border-gray20 rounded-full w-6 h-6"></li>
-          <li className="border border-gray20 rounded-full w-6 h-6"></li>
+          {item.linkList?.map((item, index) => (
+            <li
+              key={`${item}-${index}`}
+              className="border border-gray20 rounded-full w-6 h-6">
+              <Link href={item.url}>
+                <Icons icon={item.type} />
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/components/container/ProjectCardContainer.tsx
+++ src/components/container/ProjectCardContainer.tsx
@@ -1,22 +1,76 @@
-import ProjectCard from '@/components/card/ProjectCard';
-import { card, images } from '@/utils/data';
+import { useCallback, useRef } from 'react';
 
-export default function ProjectCardContainer() {
+import ProjectCard from '@/components/card/ProjectCard';
+import useProject from '@/hooks/useProject';
+
+import { IProjectType } from '../../types/global';
+import SkeletonProjectCard from '../skeleton/SkeletonProjectCard';
+
+interface IProjectCardContainerProps {
+  category?: string;
+}
+
+export default function ProjectCardContainer({
+  category = 'all',
+}: IProjectCardContainerProps) {
+  const { isError, data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useProject(category);
+
+  const observer: React.MutableRefObject<IntersectionObserver | null> =
+    useRef(null);
+
+  const lastElementRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (isFetchingNextPage) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting && !isError) {
+            fetchNextPage();
+          }
+        },
+        { threshold: 0.7 }
+      );
+      if (node) observer.current.observe(node);
+    },
+    [isFetchingNextPage]
+  );
+
   return (
     <div className="max-w-max_w w-full">
       <div className="grid grid-cols-4 gap-y-6 gap-x-3 tablet:grid-cols-2 mobile:grid-cols-1">
-        {images.map((item, index) => (
-          <ProjectCard
-            key={`projectCard-${index}`}
-            image={item}
-            id="123"
-            type="project"
-            title={card.title}
-            content={card.content}
-            tagName="팀 프로젝트"
-            tagCategory="team"
-          />
-        ))}
+        {isFetchingNextPage
+          ? [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
+              <SkeletonProjectCard key={`${item}-${index}`} />
+            ))
+          : data?.pages.map((pages: IProjectType[]) => {
+              return pages.map((item, index) => {
+                if (pages.length === index + 1) {
+                  return (
+                    <div
+                      ref={lastElementRef}
+                      key={`projectCard-${index}`}
+                      className="border-blue-500 border">
+                      <ProjectCard
+                        item={item}
+                        tagName="팀 프로젝트"
+                        tagCategory="team"
+                      />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={`projectCard-${index}`}>
+                      <ProjectCard
+                        item={item}
+                        tagName="팀 프로젝트"
+                        tagCategory="team"
+                      />
+                    </div>
+                  );
+                }
+              });
+            })}
       </div>
     </div>
   );
