40e205952ad9c5c134583d1bf857718c21079be8
springhana <xksxksanfro7@naver.com>
2024-03-06 05:16:21 +0900

    ✨feat:  프로젝트 디테일 커스텀 훅 구현 -프로젝트 아이디 없으면 요청 안하게 해야 함

---
+++ src/hooks/useProjectDetail.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const useProjectDetail = (id: string) => {
+  const { isLoading, isError, data, isFetching } = useQuery({
+    queryKey: [`project-${id}`],
+    queryFn: async () => {
+      //   if (!id) {
+      //     return;
+      //   }
+
+      const respones = await axios.get(`${process.env.BASE_URL}/project/${id}`);
+
+      if (respones.data.success) {
+        return respones.data.data;
+      }
+    },
+    gcTime: 30000 * 5, // 30분
+    staleTime: 30000 * 5, // 30분
+  });
+
+  return { isLoading, isError, data, isFetching };
+};
+
+export default useProjectDetail;
