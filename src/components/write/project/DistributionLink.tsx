37addfff8adb918e37bb3a6ae5b781fd64723627
springhana <xksxksanfro7@naver.com>
2024-03-09 04:19:46 +0900

    🔨modify:  배포 링크 배열로 전역 상태 저장
    Ref: #70

--- src/components/write/project/DistributionLink.tsx
+++ src/components/write/project/DistributionLink.tsx
@@ -3,40 +3,46 @@ import { FaGlobe } from '@react-icons/all-files/fa/FaGlobe';
 import { FaGooglePlay } from '@react-icons/all-files/fa/FaGooglePlay';
 import { FaPlusCircle } from '@react-icons/all-files/fa/FaPlusCircle';
 import { IoCloseOutline } from '@react-icons/all-files/io5/IoCloseOutline';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface IDistributionLinkProps {
   className?: string;
+  setLinkDtos: (linkDtos: { type: string; url: string }[]) => void;
 }
 
 export default function DistributionLink({
   className,
+  setLinkDtos,
 }: IDistributionLinkProps) {
-  const [addLink, setAddLink] = useState<string[]>([]);
-  const [LinkNum, setLinkNum] = useState<[string, number][]>([['default', 0]]);
+  const [addLink, setAddLink] = useState<{ type: string; url: string }[]>([]);
+  const [link, setLink] = useState<{ type: string; url: string }[]>([]);
+
+  useEffect(() => {
+    const newLinkDtos = [...addLink, ...link];
+    setLinkDtos(newLinkDtos);
+  }, [addLink, link]);
 
   const handleAddLink = () => {
     if (addLink.length < 5) {
-      setAddLink([...addLink, 'default']);
-      setLinkNum([...LinkNum, ['default', 0]]);
+      setAddLink([...addLink, { type: 'default', url: 'default' }]);
     }
   };
 
   const icons = [
     {
-      id: 'webSite',
+      id: 'web',
       title: '웹 사이트',
       icon: <FaGlobe size={25} />,
       placeholder: '웹 사이트 배포 링크를 입력해주세요.',
     },
     {
-      id: 'googleStore',
+      id: 'android',
       title: '구글 스토어',
       icon: <FaGooglePlay size={25} />,
       placeholder: '구글 스토어 배포 링크를 입력해주세요.',
     },
     {
-      id: 'appleStore',
+      id: 'apple',
       title: '애플 스토어',
       icon: <FaApple size={25} />,
       placeholder: '애플 스토어 배포 링크를 입력해주세요.',
@@ -44,7 +50,31 @@ export default function DistributionLink({
   ];
 
   const handleDeleteButton = (index: number) => {
-    setAddLink(addLink.filter((_, i) => i !== index));
+    const newAddLink = addLink.filter((_, i) => i !== index);
+    setAddLink(newAddLink);
+  };
+
+  const handleChangeLinkInput = (type: string, value: string) => {
+    const newLink = [...link];
+    if (newLink.some((item) => item.type === type)) {
+      const index = newLink.findIndex((item) => item.type === type);
+      newLink[index].url = value;
+      setLink(newLink);
+    } else {
+      const newLink = [...link, { type: type, url: value }];
+      setLink(newLink);
+    }
+  };
+
+  const handleChangeInput = (
+    index: number,
+    value: string,
+    inputType: string
+  ) => {
+    const newAddLink = [...addLink];
+    if (inputType === 'type') newAddLink[index].type = value;
+    else if (inputType === 'url') newAddLink[index].url = value;
+    setAddLink(newAddLink);
   };
 
   return (
@@ -77,6 +107,9 @@ export default function DistributionLink({
               name={item.id}
               id={item.id}
               placeholder={item.placeholder}
+              onChange={(e) => {
+                handleChangeLinkInput(item.id, e.target.value);
+              }}
               className="w-full h-[3.125rem] ml-2 border border-gray30 rounded-bs_5 pl-3 text-bs_14"
             />
           </li>
@@ -94,6 +127,12 @@ export default function DistributionLink({
                 name="siteLink"
                 id="siteLink"
                 placeholder="배포 또는 참고 사이트 이름"
+                value={
+                  addLink[index].type === 'default' ? '' : addLink[index].type
+                }
+                onChange={(e) => {
+                  handleChangeInput(index, e.target.value, 'type');
+                }}
                 className="w-1/3 h-[3.125rem] mr-2 border border-gray30 rounded-bs_5 pl-3 text-bs_14"
               />
               <label htmlFor="siteLink" className="sr-only">
@@ -104,6 +143,12 @@ export default function DistributionLink({
                 name="siteLink"
                 id="siteLink"
                 placeholder="참고 사이트 링크를 입력해주세요."
+                value={
+                  addLink[index].url === 'default' ? '' : addLink[index].url
+                }
+                onChange={(e) => {
+                  handleChangeInput(index, e.target.value, 'url');
+                }}
                 className="w-full h-[3.125rem] mr-2 border border-gray30 rounded-bs_5 pl-3 text-bs_14"
               />
               <button onClick={() => handleDeleteButton(index)}>
