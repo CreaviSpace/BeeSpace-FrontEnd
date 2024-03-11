f92241c72406ae7590c15fb4f35b47eb81de1f66
springhana <xksxksanfro7@naver.com>
2024-03-12 00:16:44 +0900

    🔨modify:  isLoading 추가 - 프로젝트 페이지 경우 fetchNextPage 대신 isLoading 사용

--- src/hooks/useProject.ts
+++ src/hooks/useProject.ts
@@ -2,24 +2,31 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const useProject = (kind: string, size?: number, page?: number) => {
-  const { isError, data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: [`project-list-${kind}`],
-      queryFn: async ({ pageParam = 1 }) => {
-        const response = await axios.get(
-          `${process.env.BASE_URL}/project?size=${16}&page=${pageParam}&kind=${kind}`
-        );
-        if (response.data.success) {
-          return response.data.data;
-        }
-      },
-      staleTime: 30000 * 6, // 30분
-      gcTime: 30000 * 6, // 30분
-      initialPageParam: 1,
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    });
+  const {
+    isLoading,
+    isError,
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: [`project-list-${kind}`],
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await axios.get(
+        `${process.env.BASE_URL}/project?size=${16}&page=${pageParam}&kind=${kind}`
+      );
+      if (response.data.success) {
+        return response.data.data;
+      }
+    },
+    staleTime: 30000 * 6, // 30분
+    gcTime: 30000 * 6, // 30분
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
 
   return {
+    isLoading,
     isError,
     data,
     fetchNextPage,
