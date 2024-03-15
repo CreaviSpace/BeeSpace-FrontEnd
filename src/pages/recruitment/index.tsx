84336ff02b1423a2f908cdd4b7d1f3a7d12549dd
rerro <101159509+rorre0@users.noreply.github.com>
2024-03-15 17:12:24 +0900

    🔨modify: 모집, 커뮤니티 페이지 배너 관련 수정
    - 모집 배너 추가
    - 커뮤니티 배너 삭제

--- src/pages/recruitment/index.tsx
+++ src/pages/recruitment/index.tsx
@@ -4,7 +4,10 @@ import Banner from '@/components/banner/Banner';
 import Category from '@/components/Category';
 import RecruitmentCardContainer from '@/components/container/RecruitmentCardContainer';
 
-export default function Recruitment() {
+interface IRecruitKind {
+  recruitment: string;
+}
+export default function Recruitment({ recruitment }: IRecruitKind) {
   const category = [
     { name: '프로젝트', link: 'project' },
     { name: '스터디', link: 'study' },
@@ -16,11 +19,11 @@ export default function Recruitment() {
   return (
     <main>
       <section>
-        <Banner kind="recruit" />
+        <Banner postType="recruitment" />
       </section>
       <Category category={category} btnValue="프로젝트 올리기" />
       <section className="flex justify-center pt-14 pb-24">
-        <RecruitmentCardContainer />
+        <RecruitmentCardContainer postType={type as string} />
       </section>
     </main>
   );
