f4eafc43a1f723c7231b6d6466563a6f0d277916
springhana <xksxksanfro7@naver.com>
2024-03-17 20:40:13 +0900

    🔨modify:  로그인 link 수정 - API 호출로 로그인

--- src/components/modals/LogInModal.tsx
+++ src/components/modals/LogInModal.tsx
@@ -3,7 +3,6 @@ import { FcGoogle } from '@react-icons/all-files/fc/FcGoogle';
 import { IoLogoGithub } from '@react-icons/all-files/io/IoLogoGithub';
 import { RiKakaoTalkFill } from '@react-icons/all-files/ri/RiKakaoTalkFill';
 import Image from 'next/image';
-import Link from 'next/link';
 
 import useLoginModal from '@/store/useLoginModal';
 
@@ -17,19 +16,19 @@ export default function LogInModal() {
       title: '구글',
       icon: <FcGoogle key="google" size={45} />,
       style: 'bg-white',
-      link: '/',
+      link: () => {},
     },
     {
       title: '카카오',
       icon: <RiKakaoTalkFill key="kakao" size={45} />,
       style: 'bg-yellow-300',
-      link: '/',
+      link: () => {},
     },
     {
       title: '깃허브',
       icon: <IoLogoGithub key="github" size={45} />,
       style: 'bg-white',
-      link: '/',
+      link: () => {},
     },
     {
       title: '네이버',
@@ -43,7 +42,11 @@ export default function LogInModal() {
         />
       ),
       style: 'bg-green-500 p-3 flex',
-      link: '/',
+      link: async () => {
+        // await axios.get(
+        //   'https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/login/oauth2/authorization/naver'
+        // );
+      },
     },
   ];
   return (
@@ -53,16 +56,16 @@ export default function LogInModal() {
       <ModalBody>
         <section className="mb-11 m-auto flex justify-between gap-10">
           {icons.map((icon, index) => (
-            <Link
+            <button
               key={`icon-${index}`}
-              href={icon.link}
-              className=" flex flex-col items-center gap-2">
+              className=" flex flex-col items-center gap-2"
+              onClick={icon.link}>
               <div
                 className={`${icon.style} rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2`}>
                 {icon.icon}
               </div>
               <span>{icon.title}</span>
-            </Link>
+            </button>
           ))}
         </section>
       </ModalBody>
