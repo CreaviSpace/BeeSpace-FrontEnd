bc92ec379369ed17afcfd0d94c447eecd9f878cf
springhana <xksxksanfro7@naver.com>
2024-03-26 01:14:46 +0900

    🔨modify:  재확인 모달창 데이터 추가

--- src/store/useReconfirmModal.ts
+++ src/store/useReconfirmModal.ts
@@ -2,14 +2,22 @@ import { create } from 'zustand';
 
 interface IReconfirmModalProps {
   isOpen: boolean;
+  id: number;
+  postType: string;
   onOpen: () => void;
   onClose: () => void;
+  setPostType: (postType: string) => void;
+  setId: (id: number) => void;
 }
 
 const useReconfirmModal = create<IReconfirmModalProps>((set) => ({
   isOpen: false,
+  postType: '',
+  id: 0,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
+  setPostType: (postType: string) => set({ postType }),
+  setId: (id: number) => set({ id }),
 }));
 
 export default useReconfirmModal;
