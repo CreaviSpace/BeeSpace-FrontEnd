0b00e66d473d7d7ac08af0163d94ac469da9c11f
springhana <xksxksanfro7@naver.com>
2024-03-29 22:09:24 +0900

    🔨modify:  피드백 질문 삭제 기능 추가

--- src/components/feedback/DeleteButton.tsx
+++ src/components/feedback/DeleteButton.tsx
@@ -1,8 +1,13 @@
 import { FaTrashAlt } from '@react-icons/all-files/fa/FaTrashAlt';
-export default function DeleteButton() {
-  const handleDelete = () => {};
+
+interface IHandleDeleteProps {
+  handleDelete: () => void;
+}
+export default function DeleteButton({ handleDelete }: IHandleDeleteProps) {
   return (
-    <button className="absolute -right-[34px] top-[.625rem] border border-gray30 rounded-e-bs_5 bg-white p-[6px]">
+    <button
+      onClick={handleDelete}
+      className="absolute -right-[34px] top-[.625rem] border border-gray30 rounded-e-bs_5 bg-white p-[6px]">
       <FaTrashAlt size={20} />
     </button>
   );
