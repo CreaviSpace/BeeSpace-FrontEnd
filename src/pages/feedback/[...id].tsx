ca2ef6e6b7b5aaa3e26022b5d756ed2f8bcf41e3
springhana <xksxksanfro7@naver.com>
2024-03-29 22:11:41 +0900

    🔨modify:  질문 답변 하는 페이지로 수정

--- src/pages/feedback/[...id].tsx
+++ src/pages/feedback/[...id].tsx
@@ -1,109 +1,68 @@
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 import CustomButton from '@/components/button/CustomButton';
-import CheckBoxQuestion from '@/components/feedback/CheckBoxQuestion';
-import MultipleChoiceQuestion from '@/components/feedback/MultipleChoiceQuestion';
-import QuestionBox from '@/components/feedback/QuestionBox';
-import ShortAnswerQuestion from '@/components/feedback/ShortAnswerQuestion';
-import { IquestionType } from '@/types/global';
+import CheckBoxAnswer from '@/components/feedback/answer/CheckBoxAnswer';
+import MultipleChoiceAnswer from '@/components/feedback/answer/MultipleChoiceAnswer';
+import ShortAnswerAnswer from '@/components/feedback/answer/ShortAnswerAnswer';
+import useFeedBackGet from '@/hooks/feedback/useFeedBackGet';
+import useAnswerData from '@/store/feedback/useAnswerData';
+import { IQuestionAnswerType } from '@/types/global';
 
-export default function Feedback() {
-  const [questions, setQuestions] = useState<IquestionType[]>([
-    { question: '', type: 'multipleChoice', chiceItems: [] },
-    { question: '', type: 'short', chiceItems: [] },
-    { question: '', type: 'checkBox', chiceItems: [] },
-  ]);
+export default function FeedBackList() {
+  const router = useRouter();
+  const { id } = router.query;
 
-  const questionTypes = [
-    {
-      text: '객관식',
-      onClick: function handleMultipleChoice() {
-        const newQuestions = [...questions];
-        newQuestions.push({
-          question: '',
-          type: 'multipleChoice',
-          chiceItems: [],
-        });
-        setQuestions(newQuestions);
-      },
-    },
-    {
-      text: '단답형',
-      onClick: function handleShortAnswer() {
-        const newQuestions = [...questions];
-        newQuestions.push({ question: '', type: 'short', chiceItems: [] });
-        setQuestions(newQuestions);
-      },
-    },
-    {
-      text: '체크박스',
-      onClick: function handleCheckBox() {
-        const newQuestions = [...questions];
-        newQuestions.push({ question: '', type: 'checkBox', chiceItems: [] });
-        setQuestions(newQuestions);
-      },
-    },
-  ];
+  const { answer, setAnswer } = useAnswerData();
 
-  const handelChangeQuestion = (currentIndex: number, value: string) => {
-    const newQuestions = [...questions];
-    newQuestions[currentIndex].question = value;
-    setQuestions(newQuestions);
+  const { isLoading, isError, data, isFetching } = useFeedBackGet(
+    parseInt(id as string)
+  );
+
+  const handleQuestionUpdate = () => {
+    router.push(`/feedback/question/${id as string}?update=t`);
   };
 
-  const handelQuestionDelete = (currentIndex: number) => {
-    const newQuestion = questions.filter((_, index) => index !== currentIndex);
-    setQuestions(newQuestion);
+  const handleCancel = () => {
+    router.back();
   };
 
   return (
     <main className="bg-blue10 py-10 w-full h-full min-h-[calc(100vh-4rem-250px)]">
       <section className="max-w-[48rem] m-auto relative">
-        <h1 className="text-3xl font-semibold mb-4">피드백</h1>
-        <QuestionBox questionTypes={questionTypes} />
         <div className="flex flex-col gap-3">
-          {questions.map((item, index) => {
-            if (item.type === 'multipleChoice') {
-              return (
-                <MultipleChoiceQuestion
-                  key={`${item.type}-${index}`}
-                  questions={questions}
-                  setQuestions={setQuestions}
-                  currentIndex={index}
-                  handelChangeQuestion={handelChangeQuestion}
-                  handelQuestionDelete={handelQuestionDelete}
-                />
-              );
-            } else if (item.type === 'short') {
-              return (
-                <ShortAnswerQuestion
-                  key={`${item.type}-${index}`}
-                  questions={questions}
-                  setQuestions={setQuestions}
-                  currentIndex={index}
-                  handelChangeQuestion={handelChangeQuestion}
-                  handelQuestionDelete={handelQuestionDelete}
-                />
-              );
-            } else if (item.type === 'checkBox') {
-              return (
-                <CheckBoxQuestion
-                  key={`${item.type}-${index}`}
-                  questions={questions}
-                  setQuestions={setQuestions}
-                  currentIndex={index}
-                  handelChangeQuestion={handelChangeQuestion}
-                  handelQuestionDelete={handelQuestionDelete}
-                />
-              );
-            }
-          })}
+          {isLoading
+            ? '로딩중'
+            : data?.map((item: IQuestionAnswerType, index: number) => {
+                if (item.questionType === '주관식') {
+                  return <ShortAnswerAnswer key={index} question={item} />;
+                } else if (item.questionType === '객관식') {
+                  return <MultipleChoiceAnswer key={index} question={item} />;
+                } else if (item.questionType === '체크박스') {
+                  return <CheckBoxAnswer key={index} question={item} />;
+                }
+              })}
         </div>
-        <div className="flex justify-end mt-8 gap-2">
-          <CustomButton className="py-2 px-3 bg-white">취소</CustomButton>
-          <CustomButton className="py-2 px-3" color="secondary">
-            저장
+        <div className="flex justify-between items-center mt-8 gap-2">
+          <CustomButton
+            className="py-2 px-3"
+            color="secondary"
+            onClick={handleQuestionUpdate}>
+            수정
           </CustomButton>
+
+          <div>
+            <CustomButton
+              className="py-2 px-3 mr-2 bg-white"
+              onClick={handleCancel}>
+              취소
+            </CustomButton>
+            <CustomButton
+              color="secondary"
+              className="py-2 px-3"
+              onClick={handleCancel}>
+              제출
+            </CustomButton>
+          </div>
         </div>
       </section>
     </main>
