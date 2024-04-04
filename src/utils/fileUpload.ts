c29ef6d9d861421597aab328d1de46e1856ddc74
springhana <xksxksanfro7@naver.com>
2024-04-05 00:15:25 +0900

    🔨modify:  setCompressedImage에 옵셔널 체이닝 추가

--- src/utils/fileUpload.ts
+++ src/utils/fileUpload.ts
@@ -4,13 +4,13 @@ import { getCookies } from '@/utils/getCookies';
 
 const fileUpload = async (
   compressedImage: Blob | undefined,
-  setCompressedImage: (compressedImage: string | null) => void
+  setCompressedImage?: (compressedImage: string | null) => void
 ) => {
   // 이미지 서버 저장 로직
   if (!compressedImage) return;
 
   const imageUrl = URL.createObjectURL(compressedImage);
-  setCompressedImage(imageUrl);
+  if (setCompressedImage) setCompressedImage(imageUrl);
 
   const formData = new FormData();
   formData.append('file', compressedImage);
