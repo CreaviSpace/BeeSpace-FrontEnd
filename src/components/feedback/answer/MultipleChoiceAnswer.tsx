50af2cdb6c72504b6b75b594ff7a3561a4114d55
springhana <xksxksanfro7@naver.com>
2024-04-05 00:50:34 +0900

    🔨modify:  피드백 답변 전역상태관리에 저장

--- src/components/feedback/answer/MultipleChoiceAnswer.tsx
+++ src/components/feedback/answer/MultipleChoiceAnswer.tsx
@@ -1,20 +1,29 @@
-import { IQuestionAnswerType } from '@/types/global';
+import { IAnswerType, IQuestionAnswerType } from '@/types/global';
+
+import AnswerTitle from '../AnswerTitle';
 
 interface ICheckBoxAnswerProps {
   question: IQuestionAnswerType;
+  answer: IAnswerType[];
+  setAnswer: (answer: IAnswerType[]) => void;
+  currentIndex: number;
 }
 
 export default function MultipleChoiceAnswer({
   question,
+  answer,
+  setAnswer,
+  currentIndex,
 }: ICheckBoxAnswerProps) {
+  const handleChangeCheck = (id: number) => {
+    const newAnswer = [...answer];
+    newAnswer[currentIndex].selectedItems = [{ id: id }];
+    setAnswer(newAnswer);
+  };
+
   return (
     <div className="w-full border border-gray30 rounded-bs_5 py-6 px-8 bg-white relative">
-      <label htmlFor="MultipleChoiceQuestionInput" className="sr-only">
-        질문
-      </label>
-      <h2 className="w-full border-b border-gray20 p-2 bg-[#f7f7f7]">
-        {question.question}
-      </h2>
+      <AnswerTitle question={question.question} />
 
       <label htmlFor="MultipleChoiceQuestionInput" className="sr-only">
         대답
@@ -25,7 +34,11 @@ export default function MultipleChoiceAnswer({
             <input
               type="radio"
               id={question.question}
-              name={question.question}></input>
+              name={question.question}
+              onChange={() => handleChangeCheck(item.id)}
+              checked={
+                answer[currentIndex]?.selectedItems[0].id === item.id
+              }></input>
             <p className="w-full border-b border-gray20 p-2">{item.item}</p>
           </li>
         ))}
