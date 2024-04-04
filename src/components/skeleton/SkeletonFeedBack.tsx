a6246986eb826407bad36faaf4af79154aa8bc10
springhana <xksxksanfro7@naver.com>
2024-04-05 00:47:38 +0900

    ✨feat:  스켈레톤 피드백 구현

---
+++ src/components/skeleton/SkeletonFeedBack.tsx
@@ -0,0 +1,8 @@
+export default function SkeletonFeedBack() {
+  return (
+    <>
+      <div className="w-full border border-gray10 rounded-bs_5 py-20 px-8 bg-[#f2f2f2] relative"></div>{' '}
+      <div className="w-full border border-gray10 rounded-bs_5 py-20 px-8 bg-[#f2f2f2] relative"></div>
+    </>
+  );
+}
