165aef359638016ec8d47b1b823d996e37c1adac
springhana <xksxksanfro7@naver.com>
2024-04-05 00:16:04 +0900

    🔨modify:  신고 전역 상태에 제목 추가

--- src/store/useReportModal.ts
+++ src/store/useReportModal.ts
@@ -2,14 +2,18 @@ import { create } from 'zustand';
 
 interface IReportModalProps {
   isOpen: boolean;
+  reportTitle: string;
   onOpen: () => void;
   onClose: () => void;
+  setReportTitle: (reportTitle: string) => void;
 }
 
 const useReportModal = create<IReportModalProps>((set) => ({
   isOpen: false,
+  reportTitle: '',
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
+  setReportTitle: (reportTitle: string) => set(() => ({ reportTitle })),
 }));
 
 export default useReportModal;
