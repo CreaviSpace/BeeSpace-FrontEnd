305c07eb2c61b9c3991ca5f02dbe2b4a18e98e86
rerro <101159509+rorre0@users.noreply.github.com>
2024-04-07 01:51:20 +0900

    🔨modify:  커뮤니티 페이지 인기 태그 추가

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,6 +1,7 @@
 import Category from '@/components/Category';
 import CommunityCardContainer from '@/components/container/CommunityCardContainer';
 import PopularTag from '@/components/PopularHashTag';
+import { usePopularTag } from '@/hooks/usePopularTag';
 export default function Community() {
   const categories = [
     {
@@ -16,12 +17,14 @@ export default function Community() {
       link: 'worry',
     },
   ];
+  const { data, isLoading } = usePopularTag();
+
   return (
     <main>
       <h1 className="sr-only">커뮤니티 페이지</h1>
       <Category category={categories} btnValue="커뮤니티 글쓰기"></Category>
-      <section className="max-w-max_w flex justify-between pt-3 pb-24 m-auto">
-        <PopularTag />
+      <section className="max-w-max_w flex justify-between pt-3 pb-24 m-auto tablet:px-8 mobile:px-8">
+        {isLoading ? <></> : <PopularTag tags={data} />}
         <CommunityCardContainer size={6} />
       </section>
     </main>
