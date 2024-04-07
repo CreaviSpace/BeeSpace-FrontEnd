5301a9e4f8676f43d2be2b8313a5e3aef07eb764
springhana <xksxksanfro7@naver.com>
2024-04-07 20:38:53 +0900

    🔨modify:  디코딩 가능하게 구현

--- src/utils/getCookies.ts
+++ src/utils/getCookies.ts
@@ -1,7 +1,13 @@
 import { Cookies } from 'react-cookie';
 
-export const getCookies = (cookie: string) => {
+export const getCookies = (cookieValue: string, decoding?: boolean) => {
   const cookies = new Cookies();
 
-  return cookies.get(cookie);
+  const cookie = cookies.get(cookieValue);
+
+  if (decoding && cookie) {
+    return atob(cookie);
+  }
+
+  return cookie;
 };
