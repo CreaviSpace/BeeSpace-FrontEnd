766896a58d44157029fb0000b308165cd10100e6
springhana <xksxksanfro7@naver.com>
2024-04-10 22:10:57 +0900

    🔨modify:  input 태그 복구

--- src/components/write/project/ImageCropper.tsx
+++ src/components/write/project/ImageCropper.tsx
@@ -9,7 +9,7 @@ interface PropsType {
   children: React.ReactNode;
   image: string | null;
   setImage: (image: string | null) => void;
-  handleChildrenClick: () => void;
+  setImageName: (imageName: string) => void;
 }
 
 const ImageCropper = ({
@@ -18,9 +18,28 @@ const ImageCropper = ({
   onCrop,
   image,
   setImage,
-  handleChildrenClick,
+  setImageName,
 }: PropsType) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const cropperRef = useRef<ReactCropperElement>(null);
+
+  const handleChildrenClick = () => {
+    if (inputRef.current) inputRef.current.click();
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const files = e.target.files;
+
+    if (!files || files.length === 0) return;
+    setImageName(files[0].name);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result as string);
+    };
+    reader.readAsDataURL(files[0]);
+  };
 
   const getCropData = () => {
     if (typeof cropperRef.current?.cropper !== 'undefined') {
@@ -31,6 +50,13 @@ const ImageCropper = ({
 
   return (
     <>
+      <input
+        type="file"
+        id="Imagefile"
+        ref={inputRef}
+        onChange={handleFileChange}
+        className="sr-only"
+      />
       <label
         htmlFor="Imagefile"
         onClick={(e) => {
