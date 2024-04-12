c21d10d9abd8532fbc929449106da4a7005deb7f
springhana <xksxksanfro7@naver.com>
2024-04-13 04:36:03 +0900

    🔨modify:  size 와 className 추가

--- src/components/modals/Modals.tsx
+++ src/components/modals/Modals.tsx
@@ -10,15 +10,29 @@ interface IModalsProps extends ModalProps {
   isOpen: boolean;
   onClose: () => void;
   children: ReactNode;
+  className?: string;
+  size?: string;
 }
 
-export default function Modals({ isOpen, onClose, children }: IModalsProps) {
+export default function Modals({
+  isOpen,
+  onClose,
+  children,
+  className,
+  size,
+}: IModalsProps) {
   const finalRef = useRef(null);
 
   return (
-    <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+    <Modal
+      finalFocusRef={finalRef}
+      isOpen={isOpen}
+      onClose={onClose}
+      size={size}>
       <ModalOverlay />
-      <ModalContent>{children}</ModalContent>
+      <ModalContent className={`${className} min-w-min_w`}>
+        {children}
+      </ModalContent>
     </Modal>
   );
 }
