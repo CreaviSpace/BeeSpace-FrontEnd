a74008b27fd01d9e6fec402fec5ce96c21d57bdc
springhana <xksxksanfro7@naver.com>
2024-04-13 04:33:24 +0900

    🔨modify:  반응형 추가

--- src/components/modals/LogInModal.tsx
+++ src/components/modals/LogInModal.tsx
@@ -4,65 +4,82 @@ import { IoLogoGithub } from '@react-icons/all-files/io/IoLogoGithub';
 import { RiKakaoTalkFill } from '@react-icons/all-files/ri/RiKakaoTalkFill';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import useLoginModal from '@/store/useLoginModal';
 
 import Modals from './Modals';
 
+const ICONS = [
+  {
+    title: '구글',
+    icon: <FcGoogle key="google" size={45} />,
+    style: 'bg-white',
+    link: 'https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/oauth2/authorization/google',
+  },
+  {
+    title: '카카오',
+    icon: <RiKakaoTalkFill key="kakao" size={45} />,
+    style: 'bg-yellow-300',
+    link: '/',
+  },
+  {
+    title: '깃허브',
+    icon: <IoLogoGithub key="github" size={45} />,
+    style: 'bg-white',
+    link: '/',
+  },
+  {
+    title: '네이버',
+    icon: (
+      <Image
+        src="/img/icon/SiNaver.svg"
+        alt="네이버 아이콘"
+        width={35}
+        height={35}
+        className="object-center"
+      />
+    ),
+    style: 'bg-green-500 p-3 flex',
+    link: 'https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/oauth2/authorization/naver',
+  },
+];
+
 export default function LogInModal() {
   const { isOpen, onOpen, onClose } = useLoginModal();
+  const [modalSize, setModalSize] = useState('md');
 
-  const icons = [
-    {
-      title: '구글',
-      icon: <FcGoogle key="google" size={45} />,
-      style: 'bg-white',
-      link: 'https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/oauth2/authorization/google',
-    },
-    {
-      title: '카카오',
-      icon: <RiKakaoTalkFill key="kakao" size={45} />,
-      style: 'bg-yellow-300',
-      link: '/',
-    },
-    {
-      title: '깃허브',
-      icon: <IoLogoGithub key="github" size={45} />,
-      style: 'bg-white',
-      link: '/',
-    },
-    {
-      title: '네이버',
-      icon: (
-        <Image
-          src="/img/icon/SiNaver.svg"
-          alt="네이버 아이콘"
-          width={35}
-          height={35}
-          className="object-center"
-        />
-      ),
-      style: 'bg-green-500 p-3 flex',
-      link: 'https://port-0-creavispace-backend-am952nlsse11uk.sel5.cloudtype.app/oauth2/authorization/naver',
-    },
-  ];
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 768) {
+        setModalSize('full');
+      } else {
+        setModalSize('md');
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <Modals isOpen={isOpen} onClose={onClose}>
+    <Modals isOpen={isOpen} onClose={onClose} size={modalSize}>
       <ModalCloseButton />
       <ModalHeader className="text-center mt-10 mb-2">로그인</ModalHeader>
       <ModalBody>
-        <section className="mb-11 m-auto flex justify-between gap-10">
-          {icons.map((icon, index) => (
+        <section className="mb-11 m-auto flex justify-between gap-10 mobile:flex-col">
+          {ICONS.map((icon, index) => (
             <Link
               key={`icon-${index}`}
               href={icon.link}
-              className=" flex flex-col items-center gap-2">
+              className="relative flex flex-col items-center gap-2 mobile:flex-row mobile:w-full">
               <div
-                className={`${icon.style} rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2`}>
+                className={`${icon.style} rounded-full shadow-[0_3px_5px_rgba(0,0,0,0.3)] p-2 mobile:rounded-sm mobile:w-full mobile:flex mobile:justify-center`}>
                 {icon.icon}
               </div>
-              <span>{icon.title}</span>
+              <span className="mobile:hidden">{icon.title}</span>
             </Link>
           ))}
         </section>
