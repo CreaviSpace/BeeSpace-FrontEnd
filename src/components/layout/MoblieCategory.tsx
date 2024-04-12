f9795dd0e5d1757377424e0a1089433af3b336ff
springhana <xksxksanfro7@naver.com>
2024-04-13 04:31:09 +0900

    🔨modify:  토글 핸들러 props로 추가

--- src/components/layout/MoblieCategory.tsx
+++ src/components/layout/MoblieCategory.tsx
@@ -2,21 +2,28 @@ import Link from 'next/link';
 
 interface IMoblieCategoryProps {
   menu: { name: string; link: string }[];
+  handleMenuToggle: () => void;
 }
 
-export default function MoblieCategory({ menu }: IMoblieCategoryProps) {
+export default function MoblieCategory({
+  menu,
+  handleMenuToggle,
+}: IMoblieCategoryProps) {
   return (
-    <ul className="absolute top-[4rem] left-0 w-full h-screen shadow-md ">
+    <ul className="absolute top-[4rem] left-0 w-full h-screen shadow-md hidden mobile:block">
       {menu.map((item) => (
         <li key={item.name} className="bg-[#F5F5F5] border-b border-gray10">
           <Link
             href={item.link}
-            className="p-5 text-center block hover:text-primary">
+            className="p-5 text-center block hover:text-primary"
+            onClick={handleMenuToggle}>
             {item.name}
           </Link>
         </li>
       ))}
-      <div className=" h-full bg-black/50 w-full cursor-pointer"></div>
+      <div
+        className="h-screen bg-black/50 w-full cursor-pointer"
+        onClick={handleMenuToggle}></div>
     </ul>
   );
 }
