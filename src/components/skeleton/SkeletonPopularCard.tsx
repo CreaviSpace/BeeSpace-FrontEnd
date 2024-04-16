e3ec8f3186224654028ab5ed712bc741f0fc5788
rerro <101159509+rorre0@users.noreply.github.com>
2024-04-16 18:51:22 +0900

    🎨design:  인기프로젝트 스켈레톤 ui 수정

--- src/components/skeleton/SkeletonPopularCard.tsx
+++ src/components/skeleton/SkeletonPopularCard.tsx
@@ -1,7 +1,6 @@
 export default function SkeletonPopularCard() {
   return (
     <div className="w-[550px] p-[0.1px] mx-auto mb-auto overflow-x-hidden tablet:w-[767px] min_mobile:w-[330px]">
-      <h2 className="text-bs_24 font-bold">인기 프로젝트</h2>
       <div
         className={`relative right-0 my-5 h-80 w-[34.375rem] tablet:w-[47.9375rem] tablet:h-[27.875rem] min_mobile:w-[20.625rem]  min_mobile:h-[11.625rem] p-1 rounded-bs_10 bg-[#ECECEC] skeleton-list-item`}></div>
       <div
