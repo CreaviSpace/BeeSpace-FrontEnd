d0ac5b88b66e0aa8a789f6ea387c4e972ec7d443
springhana <xksxksanfro7@naver.com>
2024-04-20 23:40:57 +0900

    🔨modify:  logout 추가

--- src/store/useLogin.ts
+++ src/store/useLogin.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 interface IuseLoginProps {
   login: boolean;
   setLogin: () => void;
+  setLogout: () => void;
 }
 
 const useLogin = create<IuseLoginProps>((set) => ({
