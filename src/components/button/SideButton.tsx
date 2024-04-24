e4a50648ada874be75282d6da8731c324db909c4
springhana <xksxksanfro7@naver.com>
2024-04-24 14:58:46 +0900

    🔨modify:  onClick 수정

--- src/components/button/SideButton.tsx
+++ src/components/button/SideButton.tsx
@@ -15,29 +15,17 @@ interface ISideButtonProps {
 export default function SideButton({ id, type, className }: ISideButtonProps) {
   const [isHidden, setIsHidden] = useState(true);
 
-  const handleButtonMove = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    setIsHidden(!isHidden);
-  };
-
-  const handleButtonOnClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
+  const handleButtonOnClick = () => {
     setIsHidden(!isHidden);
   };
 
   return (
     <aside className={`${className} absolute right-10`}>
-      <div
-        className="fixed w-fit h-fit p-2 py-3 rounded-md bg-secondary z-10 tablet:p-[0.375rem] mobile:p-[0.375rem] tablet:bottom-2 tablet:right-2 mobile:bottom-2 mobile:right-2"
-        onClick={(e) => {
-          handleButtonOnClick(e);
-        }}>
+      <div className="fixed w-fit h-fit p-2 py-3 rounded-md bg-secondary z-10 tablet:p-[0.375rem] mobile:p-[0.375rem] tablet:bottom-2 tablet:right-2 mobile:bottom-2 mobile:right-2">
         <div className="flex flex-col items-center gap-2 relative">
           <button
             className={`bordere rounded-md bg-white p-3 font-black hidden z-10 bottom-0 tablet:block mobile:block tablet:p-2 mobile:p-2 ${isHidden ? 'tablet:relative mobile:relative' : 'tablet:absolute mobile:absolute'}`}
-            onClick={(e) => {
-              handleButtonMove(e);
-            }}>
+            onClick={() => handleButtonOnClick()}>
             <IoIosArrowDown
               size={25}
               className={`${isHidden ? 'rotate-0' : 'rotate-180'} transition-all`}
