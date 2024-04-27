39fd4a88b4ae2f63bfd3e9de2c66090f498c5932
rerro <101159509+rorre0@users.noreply.github.com>
2024-04-27 19:49:53 +0900

    🔨modify: 컨텐츠 삭제 기능 추가
    - 삭제 버튼 추가
    - 삭제 전 경고 모달 창 추가
    - 삭제 기능 추가

--- src/components/manager/ContentManagement.tsx
+++ src/components/manager/ContentManagement.tsx
@@ -1,7 +1,12 @@
+import { IoEllipsisHorizontalSharp } from '@react-icons/all-files/io5/IoEllipsisHorizontalSharp';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import useAdminContentGet from '@/hooks/admin/useAdminContentGet';
+import useAdminContentsDelete from '@/hooks/admin/useAdminContentsDelete';
+import useReconfirmModal from '@/store/modal/useReconfirmModal';
 import { IUniversalType } from '@/types/global';
+import { parseValue } from '@/utils/parseValue';
 
 import UniversalCard from '../card/UniversalCard';
 import SkeletonUniversalCard from '../skeleton/SkeletonUniversalCard';
@@ -9,10 +14,19 @@ import SkeletonUniversalCard from '../skeleton/SkeletonUniversalCard';
 export default function ContentManagement() {
   const router = useRouter();
   const { type } = router.query;
+  const [select, setSelect] = useState({ id: 0, type: '' });
 
   const { isLoading, data, isFetchingNextPage, hasNextPage } =
     useAdminContentGet(type as string);
 
+  const { mutate } = useAdminContentsDelete(select.id, select.type);
+  const { onOpen, setHandlerFunction, setTitle } = useReconfirmModal();
+  const handleDeleteContent = (id: number, type: string) => {
+    setSelect({ id, type });
+    setTitle(`${parseValue(type.toUpperCase())}을(를) 삭제하시겠습니까?`);
+    setHandlerFunction(mutate);
+    onOpen();
+  };
   return (
     <section className="w-full p-3">
       {isLoading ? (
@@ -21,17 +35,28 @@ export default function ContentManagement() {
         <div className="p-3 bg-blue10">
           {data?.pages.map((item) => {
             return item.map((item: IUniversalType, index: number) => (
-              <UniversalCard
-                key={`${item}-${index}`}
-                id={item.id}
-                postType={item.postType}
-                title={item.title}
-                image={item.thumbnail ? item.thumbnail : ''}
-                content={item.bannerContent ? item.bannerContent : item.content}
-                hidden={false}
-                size="large"
-                className="border-2 border-gray20 mb-2 tablet:w-full bg-white"
-              />
+              <div
+                className="mx-auto relative"
+                key={`${item.contentsTitle}-${item.postId}`}>
+                <button
+                  className="absolute z-[1] right-10 top-1"
+                  onClick={() => handleDeleteContent(item.id, item.postType)}>
+                  <IoEllipsisHorizontalSharp size={25} />
+                </button>
+                <UniversalCard
+                  key={`${item}-${index}`}
+                  id={item.id}
+                  postType={item.postType}
+                  title={item.title}
+                  image={item.thumbnail ? item.thumbnail : ''}
+                  content={
+                    item.bannerContent ? item.bannerContent : item.content
+                  }
+                  hidden={false}
+                  size="large"
+                  className="border-2 border-gray20 mb-2 tablet:w-full bg-white"
+                />
+              </div>
             ));
           })}
         </div>
