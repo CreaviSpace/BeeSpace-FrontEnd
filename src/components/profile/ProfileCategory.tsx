76a5c5c16cc40e6613b53e0daa54fea19a9265a3
rerro <101159509+rorre0@users.noreply.github.com>
2024-04-30 17:34:34 +0900

    🎨design:  선택된 메뉴에 border 추가

--- src/components/profile/ProfileCategory.tsx
+++ src/components/profile/ProfileCategory.tsx
@@ -12,7 +12,7 @@ export default function ProfileCategory({
   setSelectedTab,
 }: IProfileCategoryProps) {
   const [isToggle, setIsToggle] = useState(true);
-
+  const [isActive, setIsActive] = useState('');
   const [selectMenu, setSelectMenu] = useState('내 게시물');
 
   const handleSelectTab = (item: { type: string; name: string }) => {
@@ -24,10 +24,14 @@ export default function ProfileCategory({
     setIsToggle(!isToggle);
   };
 
+  const handleActiveMemu = (menuName: string) => {
+    setIsActive(menuName);
+  };
+
   return (
     <nav className="sticky top-16 w-full border-y bg-white border-gray30 mt-10 z-[10]">
       <div className="max-w-4xl m-auto flex justify-between items-center mobile:justify-center">
-        <ul className="w-full flex mobile:flex-col text-center gap-12 mobile:gap-3">
+        <ul className="w-full flex mobile:flex-col text-center  mobile:gap-3">
           <li className="hidden mobile:block">
             <button
               className="text-bs_20 w-full py-5 border-b border-gray30"
@@ -44,9 +48,11 @@ export default function ProfileCategory({
               onClick={() => {
                 handleSelectTab(item);
                 handleToggleMemu();
+                handleActiveMemu(item.name);
               }}
-              className={`${isToggle && 'mobile:hidden'} text-bs_20 cursor-pointer`}>
-              <button className="mobile:w-full mobile:py-5 px-2 py-8 ">
+              className={`${isToggle && 'mobile:hidden'} text-bs_20`}>
+              <button
+                className={`w-[140px] h-[4.6875rem] ${isActive === item.name && 'border-b-2 border-primary'} mobile:w-full mobile:py-5`}>
                 {item.name}
               </button>
             </li>
