0db33d1f9c2e10caa0ef493eefd53b084e2ecaad
springhana <xksxksanfro7@naver.com>
2024-05-04 19:01:09 +0900

    🔨modify:  페이지 이동 로딩 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,8 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
+import Router from 'next/router';
+import { Suspense, useEffect, useState } from 'react';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
@@ -27,6 +28,36 @@ const ToastContainer = dynamic(
 );
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | undefined;
+
+    const start = () => {
+      // 시작 시 타이머 설정
+      timer = setTimeout(() => {
+        setLoading(true);
+      }, 500); // 1초 후에 로딩 상태를 설정
+    };
+
+    const end = () => {
+      // 끝나면 타이머 클리어
+      clearTimeout(timer);
+      setLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
@@ -37,9 +68,13 @@ export default function App({ Component, pageProps }: AppProps) {
           <SignUpModal />
           <ReconfirmModal />
           <SearchErrorModal />
-          <Suspense fallback={<Loading />}>
-            <Component {...pageProps} />
-          </Suspense>
+          {loading ? (
+            <Loading />
+          ) : (
+            <Suspense fallback={<Loading />}>
+              <Component {...pageProps} />
+            </Suspense>
+          )}
           <Footer />
         </div>
         <ToastContainer position="top-center" />
