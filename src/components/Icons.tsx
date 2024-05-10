3848098519ae020a4a1701a00a9b0858ce289f84
springhana <xksxksanfro7@naver.com>
2024-05-11 01:40:12 +0900

    🔨modify:  중복 태그 수정

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,27 +3,19 @@ import { FaGlobe } from '@react-icons/all-files/fa/FaGlobe';
 import { FcGoogle } from '@react-icons/all-files/fc/FcGoogle';
 
 interface IIconProps {
-  icon: string;
+  icon?: string;
 }
 
 export default function Icons({ icon }: IIconProps) {
-  if (icon.toLowerCase() === 'android') {
-    return (
-      <div className="w-10 h-10 flex justify-center items-center rounded-full border-2 border-gray10">
-        <FcGoogle size={25} />
-      </div>
-    );
-  } else if (icon.toLowerCase() === 'ios') {
-    return (
-      <div className="w-10 h-10 flex justify-center items-center rounded-full border-2 border-gray10">
-        <FaApple size={25} />
-      </div>
-    );
-  } else if (icon.toLowerCase() == 'web') {
-    return (
-      <div className="w-10 h-10 flex justify-center items-center rounded-full border-2 border-gray10">
-        <FaGlobe size={25} />
-      </div>
-    );
+  if (!icon) {
+    return null;
   }
+
+  return (
+    <div className="w-10 h-10 flex justify-center items-center rounded-full border-2 border-gray10">
+      {icon.toLowerCase() === 'android' && <FcGoogle size={25} />}
+      {icon.toLowerCase() === 'ios' && <FaApple size={25} />}
+      {icon.toLowerCase() === 'web' && <FaGlobe size={25} />}
+    </div>
+  );
 }
