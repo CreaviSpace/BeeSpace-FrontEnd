8f94d6a0e58b922c89b7724f2735f8200d4039ad
springhana <xksxksanfro7@naver.com>
2024-05-13 17:22:24 +0900

    ✨feat:  유저 비활성화 훅 구현

---
+++ src/hooks/admin/useAdminSanction.ts
@@ -0,0 +1,20 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+import { getCookies } from '@/utils/getCookies';
+
+const useAdminSanction = (date: { memberId: string; reason: string }) => {
+  const token = getCookies('jwt');
+
+  const { mutate } = useMutation({
+    mutationFn: async () => {
+      return await axios.post(`${process.env.BASE_URL}/admin/sanction`, date, {
+        headers: { Authorization: token },
+      });
+    },
+  });
+
+  return { mutate };
+};
+
+export default useAdminSanction;
