d0d0e286d87fdf7f5f75d003a8e8e1b325f8bbc8
springhana <xksxksanfro7@naver.com>
2024-06-01 23:18:32 +0900

    ♻️refactor:  북마크 상태에 따라 분리

--- src/hooks/queries/useBookMark.ts
+++ src/hooks/queries/useBookMark.ts
@@ -1,72 +1,86 @@
+// 프로젝트, 모집, 커뮤니티 등의 게시글과 북마크, 좋아요
+
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
 
 import { queryKeys } from '@/constants/keys';
+import { errorMessages } from '@/constants/messages';
 import useLoginModal from '@/store/modal/useLoginModal';
-import { axiosInstance } from '@/utils/api/axiosInstance';
-import { getCookies } from '@/utils/cookie/getCookies';
-import { postCookies } from '@/utils/cookie/postCookies';
 import queryClient from '@/utils/queryClien';
+import { queryOnError } from '@/utils/queryOnError';
 
-const useBookMark = (id?: number, postType?: string) => {
-  const { onOpen } = useLoginModal();
-  const token = getCookies('jwt');
+import useAxiosInstance from '../useAxiosInstance';
+import useCookie from '../useCookie';
 
-  const { isLoading, isError, data, isFetching } = useQuery({
-    enabled: !!token && !!id,
-    queryKey: [queryKeys.BOOKMARK, id],
+const useGetBookmarkPost = (id: number, postType: string) => {
+  const axiosInstance = useAxiosInstance();
+  const { getCookies, setCookies } = useCookie(['jwt', 'MID']);
+
+  return useQuery({
     queryFn: async () => {
-      const response = await axiosInstance.get(
+      const { data, status } = await axiosInstance.get(
         `/bookmark?postId=${id}&postType=${postType}`
       );
 
-      if (response.status === 200 && response.data.success) {
-        return response.data.data;
-      } else if (response.status === 202 && !response.data.success) {
-        postCookies({
+      return { data, status };
+    },
+    queryKey: [queryKeys.BOOKMARK, id],
+    enabled: Boolean(id) && Boolean(getCookies('jwt')),
+    select: (response) => {
+      if (!response) return;
+
+      if (response.status === 202 && !response.data.success) {
+        // toast.error(errorMessages.TRY_AUTH_TOKEN_EXPIRED, {
+        //   onClose: () =>
+        setCookies({
           jwt: response.data.jwt,
           MID: response.data.memberId,
         });
+        // });
       }
+
+      return response.data.data;
     },
   });
+};
 
-  const { mutate } = useMutation({
+const useMutateBookmarkPost = (id: number, postType: string) => {
+  const axiosInstance = useAxiosInstance();
+  const { getCookies, setCookies } = useCookie(['jwt', 'MID']);
+  const { onOpen } = useLoginModal();
+
+  return useMutation({
     mutationFn: async () => {
-      if (!token) {
-        return onOpen();
-      } else if (!id) {
-        return;
-      }
-
       return await axiosInstance.post(
         `/bookmark?postId=${id}&postType=${postType}`
       );
     },
+    onSuccess: (response) => {
+      if (!response) return;
 
-    onSuccess: (data) => {
-      if (data) {
-        if (data.status === 200 && data.data.success) {
-          queryClient.setQueryData([queryKeys.BOOKMARK, id], data.data.data);
+      if (response.status === 200 && response.data.success) {
+        queryClient.setQueryData([queryKeys.BOOKMARK, id], response);
 
-          queryClient.invalidateQueries({
-            queryKey: [queryKeys.PROFILE_CONTENT, postType?.toLowerCase()],
-          });
-        } else if (data.status === 202 && !data.data.success) {
-          postCookies({
-            jwt: data.data.jwt,
-            MID: data.data.memberId,
-          });
-        }
+        queryClient.invalidateQueries({
+          queryKey: [queryKeys.PROFILE_CONTENT, postType?.toLowerCase()],
+        });
+      } else if (response.status === 202 && !response.data.success) {
+        toast.error(errorMessages.TRY_AUTH_TOKEN_EXPIRED, {
+          onClose: () =>
+            setCookies({
+              jwt: response.data.jwt,
+              MID: response.data.memberId,
+            }),
+        });
       }
     },
     onError: (error) => {
-      console.error(error);
-      toast.error('에러 발생');
+      queryOnError(getCookies('jwt'), error);
+    },
+    onSettled: () => {
+      if (!getCookies('jwt')) return onOpen();
     },
   });
-
-  return { isLoading, isError, data, isFetching, mutate };
 };
 
-export default useBookMark;
+export { useGetBookmarkPost, useMutateBookmarkPost };
