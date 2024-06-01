bfff2d1e4973bb8b705324cb349bee06a349489f
springhana <xksxksanfro7@naver.com>
2024-06-01 23:28:39 +0900

    ♻️refactor:  이미지 업로드 커스텀 훅으로 분리

---
+++ src/hooks/queries/useImage.ts
@@ -0,0 +1,52 @@
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+
+import { errorMessages } from '@/constants/messages';
+import useLoginModal from '@/store/modal/useLoginModal';
+import { queryOnError } from '@/utils/queryOnError';
+
+import useAxiosInstance from '../useAxiosInstance';
+import useCookie from '../useCookie';
+
+const useMutateCreateImage = () => {
+  const axiosInstance = useAxiosInstance();
+  const { getCookies, setCookies } = useCookie(['jwt', 'MID']);
+  const { onOpen } = useLoginModal();
+
+  return useMutation({
+    mutationFn: async (formData: FormData | undefined) => {
+      const { data, status } = await axiosInstance.post(
+        `/file/upload`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      return { data, status };
+    },
+    onSuccess: (response) => {
+      if (!response) return;
+
+      if (response.status === 202 && !response.data.success) {
+        toast.error(errorMessages.TRY_AUTH_TOKEN_EXPIRED, {
+          onClose: () =>
+            setCookies({
+              jwt: response.data.jwt,
+              MID: response.data.memberId,
+            }),
+        });
+      }
+    },
+    onError: (error) => {
+      queryOnError(getCookies('jwt'), error);
+    },
+    onSettled: () => {
+      if (!getCookies('jwt')) return onOpen();
+    },
+  });
+};
+
+export { useMutateCreateImage };
