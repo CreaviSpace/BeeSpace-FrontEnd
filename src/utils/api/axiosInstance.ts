003217d1b92eb83eb9436cf007f0b38aa4bdc6e7
springhana <xksxksanfro7@naver.com>
2024-06-19 01:46:05 +0900

    🔨modify:  헤더 제거

--- src/utils/api/axiosInstance.ts
+++ src/utils/api/axiosInstance.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-import { getCookies } from '../cookie/getCookies';
-
 export const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
-  headers: { Authorization: `${getCookies('jwt')}` },
 });
