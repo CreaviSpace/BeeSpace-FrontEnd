7c654b0e2156031dbf0596b78cb1cbe6ccfef962
springhana <xksxksanfro7@naver.com>
2024-06-19 17:43:02 +0900

    🔨modify:  쿼리 키 수정

--- src/hooks/queries/post/useGetInfinitePosts.ts
+++ src/hooks/queries/post/useGetInfinitePosts.ts
@@ -75,7 +75,7 @@ const useGetInfiniteCommunityPosts = (
 
     return data.data;
   };
-  const queryKey = [queryKeys.COMMUNITY, category, size];
+  const queryKey = [queryKeys.COMMUNITY, category, size, orderby && orderby];
 
   return useGetInfinitePosts(size, postsAPI, queryKey);
 };
