fb47b74eafe9348437c63d7eb928afbccc151daf
springhana <xksxksanfro7@naver.com>
2024-06-19 21:13:13 +0900

    🔨modify:  로그인을 쿼리함수에서 실행

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,20 +2,15 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import useAuth from '@/hooks/queries/useAuth';
-import useCookie from '@/hooks/useCookie';
-import useLoginStore from '@/store/useLoginStore';
 
 export default function Login() {
   const router = useRouter();
   const { getLogin } = useAuth();
   const { data: olduser, isLoading, isSuccess } = getLogin;
-  const { setLogin } = useLoginStore();
-  const { getCookies } = useCookie(['jwt']);
 
   useEffect(() => {
     if (!isLoading) {
       if (isSuccess) {
-        setLogin(getCookies('jwt'));
         if (olduser) {
           router.back();
         } else {
